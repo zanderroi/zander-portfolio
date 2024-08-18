24df71e78785f7d73f4ad1d92aa2efd0004983f0
Zander Roi Tabelona <zandertabelona@gmail.com>
2024-08-18 22:16:31 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="h-screen bg-black">
+    <header>
+      <div
+        class="fixed top-0 w-full z-20 border-b border-gray-600 bg-gray-700 text-gray-400"
+      >
+        <!-- bg-gradient-to-tr from-black via-slate-900 to-cyan-900 -->
+
+        <div class="flex">
+          <div class="border-r pt-4 border-gray-500">
+            <div
+              class="relative z-20 border-b border-orange-600 px-4 pr-20 text-left"
+            >
+              <NuxtLink
+                to="/"
+                class="flex items-center text-sm text-gray-400 text-center"
+              >
+                zander-roi-tabelona
+              </NuxtLink>
+            </div>
+          </div>
+
+          <div class="border-r pt-4 border-gray-500">
+            <div
+              class="relative z-20 px-4 text-left"
+            >
+              <NuxtLink
+                to="/"
+                class="flex items-center text-sm text-gray-400 text-center"
+              >
+                _services
+              </NuxtLink>
+            </div>
+          </div>
+
+          <div class="border-r pt-4 border-gray-500">
+            <div
+              class="relative z-20 px-4 text-left"
+            >
+              <NuxtLink
+                to="/"
+                class="flex items-center text-sm text-gray-400 text-center"
+              >
+                _gallery
+              </NuxtLink>
+            </div>
+          </div>
+
+          <div class="border-r pt-4 border-gray-500">
+            <div
+              class="relative z-20 px-4 text-left"
+            >
+              <NuxtLink
+                to="/"
+                class="flex items-center text-sm text-gray-400 text-center"
+              >
+                _about-me
+              </NuxtLink>
+            </div>
+          </div>
+
+
+          <div class="border-r pt-4 border-gray-500">
+            <div
+              class="relative z-20 px-4 text-left"
+            >
+              <NuxtLink
+                to="/"
+                class="flex items-center text-sm text-gray-400 text-center"
+              >
+                _contact-me
+              </NuxtLink>
+            </div>
+          </div>
+
+
+
+
+        </div>
+      </div>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-gradient-to-tr from-black via-slate-900 to-cyan-900">
+    <div
+      class="bg-[url('/assets/img/zandercir3.png')] h-screen bg-no-repeat bg-bottom flex flex-col justify-center p-4"
+    >
+   
+        <h1 class="text-white text-3xl">Hi, I'm Zander!</h1>
+     
+    </div>
+    <!-- <img src="/assets/img/zandercir.png" class="mx-auto" style="height: 100%;" /> -->
+  </div>
+  <div class="h-80 bg-indigo-700"></div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<style lang="scss" scoped></style>
