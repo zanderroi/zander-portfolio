ea344366c68858ab73f0214e973db8f0643a3eb2
Zander Roi Tabelona <zandertabelona@gmail.com>
2025-03-15 21:30:09 +0800

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,74 +1,82 @@
-<script setup>
-import { ref } from 'vue';
-
-const tooltipVisible = ref(false);
-const tooltipText = ref('Click to copy');
-
-const showTooltip = (text) => {
-  tooltipText.value = text;
-  tooltipVisible.value = true;
-};
-
-const hideTooltip = () => {
-  tooltipVisible.value = false;
-};
-
-const copyToClipboard = (text) => {
-  navigator.clipboard.writeText(text).then(() => {
-    tooltipText.value = 'Copied!';
-    tooltipVisible.value = true;
-    setTimeout(() => {
-      tooltipVisible.value = false;
-      tooltipText.value = 'Click to copy';
-    }, 2000);
-  });
-};
-</script>
 <template>
   <div class="bg-gray-100">
     <header>
-      <div
-        class="fixed top-0 w-full z-20 border-b border-gray-600 bg-gray-100 text-gray-400"
-      >
-        <!-- bg-gradient-to-tr from-black via-slate-900 to-cyan-900 -->
-
-        <div class="flex">
-          <div class="relative z-20 border-b-4 border-orange-600 text-center w-full p-2">
-            <p class="text-lg text-black font-semibold">Zander Roi Tabelona</p>
-            <p
-              class="text-sm text-gray-500 cursor-pointer relative"
-              @click="copyToClipboard('zandertabelona@gmail.com')"
-              @mouseover="showTooltip('Click to copy')"
-              @mouseleave="hideTooltip"
-            >
-              zandertabelona@gmail.com
-              <span
-                v-if="tooltipVisible"
-                class="absolute bottom-full mb-1 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs rounded py-1 px-2"
-              >
-                {{ tooltipText }}
-              </span>
-            </p>
-            <p>
-              <a
-                href="https://www.linkedin.com/in/zndrroi/"
-                target="_blank"
-                class="text-sm text-gray-500"
-                >linkedin.com/zndrroi</a
-              >
-            </p>
-            <p>
-              <a
-                href="https://www.github.com/zanderroi"
-                target="_blank"
-                class="text-sm text-gray-500"
-                >github.com/zndrroi</a
-              >
-            </p>
-          </div>
-        </div>
+    <div class="flex items-center justify-between p-4">
+      <div>
+        <span class="text-xl font-bold text-orange-600">ZANDER ROI TABELONA</span>
+        <button v-if="isMobile" @click="toggleDropdown" class="ml-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
       </div>
-    </header>
+      <nav v-if="!isMobile">
+
+        <p>
+          <a
+            href="https://www.linkedin.com/in/zndrroi/"
+            target="_blank"
+            class="text-sm text-gray-500"
+            >linkedin.com/zndrroi</a
+          >
+        </p>
+        <p>
+          <a
+            href="https://www.github.com/zanderroi"
+            target="_blank"
+            class="text-sm text-gray-500"
+            >github.com/zndrroi</a
+          >
+        </p>
+        <p
+          class="text-sm text-gray-500 cursor-pointer relative"
+          @click="copyToClipboard('zandertabelona@gmail.com')"
+          @mouseover="showTooltip('Click to copy')"
+          @mouseleave="hideTooltip"
+        >
+          zandertabelona@gmail.com
+          <span
+            v-if="tooltipVisible"
+            class="absolute bottom-full mb-1 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs rounded py-1 px-2"
+          >
+            {{ tooltipText }}
+          </span>
+        </p>
+      </nav>
+    </div>
+    <div v-if="isMobile && dropdownOpen" class="p-4">
+      <p
+        class="text-sm text-gray-500 cursor-pointer relative"
+        @click="copyToClipboard('zandertabelona@gmail.com')"
+        @mouseover="showTooltip('Click to copy')"
+        @mouseleave="hideTooltip"
+      >
+        zandertabelona@gmail.com
+        <span
+          v-if="tooltipVisible"
+          class="absolute bottom-full mb-1 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs rounded py-1 px-2"
+        >
+          {{ tooltipText }}
+        </span>
+      </p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/zndrroi/"
+          target="_blank"
+          class="text-sm text-gray-500"
+          >linkedin.com/zndrroi</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://www.github.com/zanderroi"
+          target="_blank"
+          class="text-sm text-gray-500"
+          >github.com/zndrroi</a
+        >
+      </p>
+    </div>
+  </header>
     <main>
       <slot />
     </main>
@@ -81,6 +89,46 @@ const copyToClipboard = (text) => {
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+
+const windowWidth = ref(typeof window !== 'undefined' ? window.innerWidth : 0);
+const dropdownOpen = ref(false);
+const tooltipVisible = ref(false);
+const tooltipText = ref('');
+
+const isMobile = computed(() => windowWidth.value < 768);
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value;
+};
+
+const showTooltip = (text) => {
+  tooltipText.value = text;
+  tooltipVisible.value = true;
+};
+
+const hideTooltip = () => {
+  tooltipVisible.value = false;
+};
+
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text);
+  showTooltip('Copied!');
+  setTimeout(hideTooltip, 2000);
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+</script>
 
 <style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-screen bg-bottom flex flex-col justify-center items-center p-4">
-    <div class="flex flex-row space-x-4 justify-center items-center p-4 my-16 md:p-4">
+  <div class="h-screen flex flex-col justify-center items-center p-2">
+    <div class="flex flex-row space-x-4 justify-center items-center p-4 md:p-4">
       <div
         class="bg-white p-8 rounded-xl max-w-3xl animate-slideInLeft h-auto shadow-xl shadow-gray-300"
       >
@@ -31,69 +31,108 @@
     </div>
   </div>
 
-  <div class="flex flex-col items-center" style="min-height: 30rem;">
+  <div class="flex flex-col items-center" style="min-height: 30rem">
     <h3 class="text-4xl text-center font-semibold text-gray-800 mb-8">Tech Stacks</h3>
 
     <div class="flex flex-wrap justify-evenly items-center mt-4 w-full space-y-2">
       <div
-        class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-6 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/css.png'" class="w-20" alt="" />
         <p class="text-center">CSS</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 5 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
       <div
-        class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-6 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/html.png'" class="w-20" alt="" />
         <p class="text-center">HTML</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 5 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
       <div
-        class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-6transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/laravel.png'" class="w-20" alt="" />
         <p class="text-center">Laravel</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 4 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
       <div
-        class="space-y-1 w-52 h-64 p-7 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-7 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/vue.png'" class="w-20" alt="" />
         <p class="text-center">VueJS</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 4 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
 
+      <div
+        class="space-y-1 w-52 h-64 p-6 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+      >
+        <img :src="'icons/wordpress.png'" class="w-20" alt="" />
+        <p class="text-center">Wordpress</p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 3 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
+      </div>
 
       <div
-        class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-6 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/aws.png'" class="w-20" alt="" />
         <p class="text-center">Amazon Web Services</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 2 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
 
       <div
-        class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        class="space-y-1 w-52 h-64 p-6 transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/mysql.png'" class="w-20" alt="" />
         <p class="text-center">MySQL</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <div class="flex justify-center">
+          <span v-for="n in 5" :key="n" class="text-yellow-500 star">
+            <i :class="n <= 4 ? 'fas fa-star' : 'far fa-star'"></i>
+          </span>
+        </div>
       </div>
     </div>
   </div>
 
   <div class="flex flex-col items-center">
-    <h3 class="text-4xl text-center font-semibold text-gray-800">Creative & Marketing Skills</h3>
+    <h3 class="text-4xl text-center font-semibold text-gray-800">
+      Creative & Marketing Skills
+    </h3>
 
     <div class="flex flex-wrap justify-evenly items-center mt-4 w-full space-y-2">
-  
       <div
         class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/pr.png'" class="w-20" alt="" />
         <p class="text-center text-xs">Adobe Premiere Pro</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
 
       <div
@@ -101,42 +140,56 @@
       >
         <img :src="'icons/ps.png'" class="w-20" alt="" />
         <p class="text-center">Adobe Photshop</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
       <div
         class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/canva.png'" class="w-20" alt="" />
         <p class="text-center">Canva</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
       <div
         class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/figma.webp'" class="w-20" alt="" />
         <p class="text-center">Figma</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
       <div
         class="space-y-1 w-52 h-64 p-7 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/vue.png'" class="w-20" alt="" />
         <p class="text-center">VueJS</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
-
 
       <div
         class="space-y-1 w-52 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow-sm transition duration-300 ease-in-out hover:scale-110 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
       >
         <img :src="'icons/vue.png'" class="w-20" alt="" />
         <p class="text-center">VueJS</p>
-        <p class="text-xs">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae blanditiis quo, </p>
+        <p class="text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+          blanditiis quo,
+        </p>
       </div>
     </div>
   </div>
 
-  <div class="px-8 py-8 flex flex-col items-center mt-4">
+  <!-- <div class="px-8 py-8 flex flex-col items-center mt-4">
     <h3 class="text-4xl text-center font-semibold text-gray-800">Work Experience</h3>
 
     <div class="flex flex-row justify-start items-center space-x-4 mt-4">
@@ -161,14 +214,160 @@
         Work 4
       </div>
     </div>
+  </div> -->
+  <!-- Carousel -->
+  <div class="px-8 py-8 flex flex-col items-center mt-4">
+    <h3 class="text-4xl text-center font-semibold text-gray-800">Projects</h3>
+    <Carousel :items-to-show="itemsToShow" :items-to-scroll="1" class="mt-4">
+      <Slide>
+        <div
+          class="box space-y-1 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-72 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        >
+          <img :src="'icons/canva.png'" class="w-20" alt="" />
+          <p class="text-center">Canva</p>
+          <p class="text-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+            blanditiis quo,
+          </p>
+          <a href="https://tara-rentals-front.vercel.app/" target="_blank" class="p-2 bg-orange-600 hover:bg-orange-500 rounded-md text-white text-xs">View Demo</a>
+        </div>
+      </Slide>
+      <Slide>
+        <div
+          class="box space-y-1 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-72 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        >
+          <img :src="'icons/canva.png'" class="w-20" alt="" />
+          <p class="text-center">Canva</p>
+          <p class="text-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+            blanditiis quo,
+          </p>
+        </div>
+      </Slide>
+      <Slide>
+        <div
+          class="box space-y-1 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-72 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        >
+          <img :src="'icons/canva.png'" class="w-20" alt="" />
+          <p class="text-center">Canva</p>
+          <p class="text-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+            blanditiis quo,
+          </p>
+        </div>
+      </Slide>
+      <Slide>
+        <div
+          class="box space-y-1 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-72 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center"
+        >
+          <img :src="'icons/canva.png'" class="w-20" alt="" />
+          <p class="text-center">Canva</p>
+          <p class="text-xs">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam repudiandae
+            blanditiis quo,
+          </p>
+        </div>
+      </Slide>
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <script setup>
 import ThreeDIcon from "@/components/ThreeDIcon.vue";
+import "vue3-carousel/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+
 definePageMeta({
   layout: "default",
 });
+
+const windowWidth = ref(typeof window !== 'undefined' ? window.innerWidth : 0);
+
+const itemsToShow = computed(() => (windowWidth.value < 768 ? 1 : 2));
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css");
+
+.star {
+  animation: glow 1s infinite alternate;
+}
+
+@keyframes glow {
+  from {
+    text-shadow: 0 0 1px yellow, 0 0 1px yellow, 0 0 2px yellow, 0 0 3px yellow;
+    /* opacity: 1; */
+  }
+  to {
+    text-shadow: 0 0 5px yellow, 0 0 5px yellow, 0 0 6px yellow, 0 0 7px yellow;
+    /* opacity: 0; */
+  }
+}
+
+.box {
+  /* width: 60vmin;
+  height: 50vmin; */
+  /* display: grid;
+  place-content: center; */
+  /* color: rgb(1, 1, 1); */
+  /* text-shadow: 0 1px 0 #000; */
+
+  --border-angle: 0turn;
+  --main-bg: conic-gradient(
+    from var(--border-angle),
+    #ffff,
+    #ffff 5%,
+    #ffff 60%,
+    #ffff 95%
+  );
+
+  border: solid 5px transparent;
+  border-radius: 2em;
+  --gradient-border: conic-gradient(
+    from var(--border-angle),
+    transparent 25%,
+    rgb(255, 72, 0),
+    rgb(255, 128, 0) 99%,
+    transparent
+  );
+
+  background: var(--main-bg) padding-box, var(--gradient-border) border-box,
+    var(--main-bg) border-box;
+
+  background-position: center center;
+
+  animation: bg-spin 3s linear infinite;
+  @keyframes bg-spin {
+    to {
+      --border-angle: 1turn;
+    }
+  }
+
+  &:hover {
+    animation-play-state: paused;
+  }
+}
+
+@property --border-angle {
+  syntax: "<angle>";
+  inherits: true;
+  initial-value: 0turn;
+}
+</style>
